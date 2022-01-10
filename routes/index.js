acda2906d23bb08e7b108cfd204fe39e94427f5e
Brandon Diaz <brandonjustindiaz@yahoo.com>
2022-01-09 20:57:21 -0500

    added more of a hello page to see what is still possible with this app

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,18 @@ const apiRoutes = require('./api');
 router.use('/api', apiRoutes);
 
 router.use((req, res) => {
-  res.send("<h1>Wrong Route!</h1>")
+  res.send(`
+  <h1>Hello!</h1>
+  <h2>Commands to try to see the data</h2>
+  <h2>/id just means any number 1-9</h2>
+  <h3>/api/tags</h3>  
+  <h3>/api/tags/id</h3>  
+  <h3>/api/products</h3> 
+  <h3>/api/products/id</h3> 
+  <h3>/api/categories</h3>
+  <h3>/api/categories/id</h3>
+  <h2>You can use insomnia in real time to update the data check the video in the readme file to get an understanding on what you can post, update, and delete on each endpoint</h2>
+   `)
 });
 
 module.exports = router;
